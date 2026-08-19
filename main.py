f59021b6b5408dc83f2394ab356f43235ce9fa8e
Nur Hamza <nurhamza888@gmail.com>
2026-08-19 07:12:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def opening():
     print("\nChoose what do you want: \n\n1. Calculator\n2. Unit Converte\n3. Password Generator\n4. Date tools\n")
     choice = int(input("What is your choice: "))
 
+    return choice
+
 if __name__ == "__main__":
     choice = opening()
     work.works(choice)
